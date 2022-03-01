cbf33e41dcf959700009e2a8106622b48268b817
Vince Suriani <chenzorama@hotmail.com>
2022-03-01 17:22:37 -0500

    404 and fallback

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import Link from 'next/dist/client/link'
+import styles from '../styles/Home.module.css'
+
+export default function FourOhFour() {
+  return (
+   
+    <div className={styles.home}>
+      <h1>Four Oh Four Page</h1>
+
+    <p>Return <Link href="things/onething">Home</Link></p>
+
+      <section>
+        <ul>
+          <li><Link href="things/onething">One Thing</Link></li>
+          <li><Link href="things/thingtwo">Thing Two</Link></li>
+          <li><Link href="things/thirdthing">Third Thing (doesn't exist)</Link></li>
+        </ul>
+      </section>
+
+
+    </div>
+
+  )
+}
---
+++ pages/things/[[...anything]].js
@@ -0,0 +1,12 @@
+import Link from 'next/dist/client/link'
+
+
+  export default function AnyThing() {
+
+
+    return (
+        <div>
+            <p>AnyThing Template - get the data for whatever this is</p>
+        </div>
+    )
+  }
